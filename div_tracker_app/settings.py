b2ef6dcf05335fa55a108890548771966562f3b1
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-16 19:41:32 -0300

    add vercel to deploy

--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yhjaz&38e1fbay^%llk-e1m=4!d&bcqx(k$v0$+l2v_9r^@&1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
 
 # Application definition
 
@@ -72,7 +72,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'div_tracker_app.wsgi.application'
+# WSGI_APPLICATION = 'div_tracker_app.wsgi.application'
+WSGI_APPLICATION = 'div_tracker_app.wsgi.app'
 
 
 # Database
