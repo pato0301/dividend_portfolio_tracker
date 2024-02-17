bcb2b6dd955f630b6049755ea3a6a801aad1450a
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-17 19:01:04 -0300

    fix build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,8 @@ echo "Building project packages..."
 python3 -m pip install -r requirements.txt
 
 echo "Migration databases..."
-python3 manage.py makemigrations --no-input
-python3 manage.py migrate --no-input
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-python3 manage.py collectstatic --no-input
+python3 manage.py collectstatic --noinput
