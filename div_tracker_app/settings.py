e5cee49dd79cf88578703172b10b660a214a0677
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-17 18:22:02 -0300

    modify vercel.json

--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -79,12 +79,14 @@ WSGI_APPLICATION = 'div_tracker_app.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {}
 
 
 # Password validation
