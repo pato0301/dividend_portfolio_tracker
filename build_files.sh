0d423efefa47e1a0bb738ebc1dec0943d52d4269
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-23 20:09:42 -0300

    minor changes

--- build_files.sh
+++ build_files.sh
@@ -7,5 +7,5 @@ echo "Migration databases..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-# echo "Collecting static files..."
+echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'dividend-portfolio-tracker.onrender.com', '.onrender.com']
 
 # Application definition
 
