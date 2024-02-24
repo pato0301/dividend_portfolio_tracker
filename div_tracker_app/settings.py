f381713e9539e7808e8a4fc48f601e6fb1b79ce1
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-24 12:27:59 -0300

    modify fiels to deploy render

--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = BASE_DIR/'assets'
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
