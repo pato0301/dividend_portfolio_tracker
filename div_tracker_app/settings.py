85c76e6e82b3296a7d7be87d17858719b38ad57c
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-02-24 12:23:06 -0300

    modify fiels to deploy render

--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.environ.get('DEBUG', 'True')=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh', 'dividend-portfolio-tracker.onrender.com', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh', 'dividend-portfolio-tracker.onrender.com', '.onrender.com']
 
 # Application definition
 
