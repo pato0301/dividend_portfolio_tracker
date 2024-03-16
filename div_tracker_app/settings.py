0a58a1fdde51ac0eee7cf201334924a610fb7e15
Patricio Villanueva <patriciovillanueva@MacBook-Pro-de-Patricio.local>
2024-03-16 17:04:28 +0100

    test agos

--- div_tracker_app/settings.py
+++ div_tracker_app/settings.py
@@ -144,6 +144,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
+# test agos
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
